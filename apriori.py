1c8c6be34a0346a6ccf270fdf109c6c7e2596498
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2025-10-05 21:04:20 -0300

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -1,1 +1,9 @@
-# This is the main executable file for the algorithm
+import sys
+
+file_name = sys.argv[1] #this is the file path we will be opening
+sup = sys.argv[2] #minimum support
+sup_percent = sup/100
+
+file = open(file_name) #this is the actual file that will be used
+
+#amount of items * sup_percent -> MST
