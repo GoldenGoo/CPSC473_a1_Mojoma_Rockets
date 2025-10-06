808f8152cc14d008a779fda5c53c72bf8de63ac9
Josh <joshholuboch@gmail.com>
2025-10-06 15:18:10 -0700

    Big group work dump
    From here, we need to fix our l table so it actually prunes, then just return all of our l table values as frequent transactions

--- apriori.py
+++ apriori.py
@@ -2,11 +2,68 @@ import sys
 from database import Database
 
 file_name = sys.argv[1] #this is the file path we will be opening
-sup = sys.argv[2] #minimum support
+sup = int(sys.argv[2]) #minimum support
 sup_percent = sup/100
+min_sup = 0
+data:Database
 
 # file = open(file_name) #this is the actual file that will be used
 
-#amount of items * sup_percent -> MST
+def apriori():
+    
+    # initializes the C0 table 
+    c_table:dict = {}
+    for transaction in data.transactions:
+        for item in transaction:
+            c_table[item] = c_table.get(item, 0) + 1
+    
+    # initialized the L0 table
+    l_table: dict = {}
+    for item in c_table:
+        if (c_table[item] >= min_sup):
+            l_table[item] = c_table[item]
+
+    make_c_table(l_table) # should recurse back and forth, updating our solution.
+
+        
+def make_c_table(l_table:dict):
+    c_table: dict = {}
+    flag: bool = False
+    for item_1 in l_table:
+        flag = False
+        for item_2 in l_table:
+            if (flag):
+                key = set(item_1).union(set(item_2))
+                key_string= "".join(list(key))
+                if (len(key_string) == len(item_1)+1):
+                    c_table[key_string] = 0  
+            if (item_1 == item_2):
+                flag = True
+    make_l_table(c_table)
+
+
+def make_l_table(c_table:dict):
+    l_table:dict = {}
+    for key in c_table:
+        for transaction in data.transactions:
+            interesect = set(key) & set(transaction)
+            if (len(set(key)) == len(interesect)):
+                l_table[key] = l_table.get(key,0) + 1
+    print(l_table)
+    if (len(l_table) > 1):
+        make_c_table(l_table)
+
 def main():
+    global data 
     data = Database(file_name)
+    frequent_patterns: list = ()
+    global min_sup
+    min_sup = int(data.size * sup_percent) #amount of items * sup_percent -> MST
+    apriori()
+
+if __name__ == "__main__":
+        main()
+
+
+#def generate_c_table(l_table:list):
+
