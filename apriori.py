7405d583013fbd27a90ecabc77ef6331d800356e
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2025-10-05 21:56:26 -0300

    Basic apriori set up

--- apriori.py
+++ apriori.py
@@ -4,6 +4,8 @@ file_name = sys.argv[1] #this is the file path we will be opening
 sup = sys.argv[2] #minimum support
 sup_percent = sup/100
 
-file = open(file_name) #this is the actual file that will be used
+# file = open(file_name) #this is the actual file that will be used
 
 #amount of items * sup_percent -> MST
+def main():
+    data = Database(file_name)
